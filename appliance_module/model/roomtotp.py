e35b022ba4ccc9ef82c3115bee66f0daeefcc041
Karolina <karolina.kov@yahoo.com>
2026-02-10 00:31:39 +0100

    second initial

---
+++ appliance_module/model/roomtotp.py
@@ -0,0 +1,16 @@
+from django.db import models
+from .room import Room
+
+
+class RoomTOTP(models.Model):
+    """
+    Represents a TOTP secret associated with a room.
+    """
+
+    room = models.OneToOneField(Room, on_delete=models.CASCADE)
+    secret = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = "room_totps"
+    def __str__(self):
+        return f"RoomTOTP(room={self.room.key}, secret={self.secret})"
